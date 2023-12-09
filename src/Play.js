9b6458c71db1de6da13e5231148bf2ed59b7ec07
Fallrain1763 <liushuchen842@icloud.com>
2023-12-08 17:08:33 -0800

    add arts and start

--- src/Play.js
+++ src/Play.js
@@ -14,23 +14,62 @@ class Play extends Phaser.Scene {
       this.load.image("fence", "./assets/museumfence.png");
     }
     create() {
-        this.gameStarted = false;
+        // Background
         this.add.sprite(0, 0, 'bg').setOrigin(0, 0)
-        //add art sprites here! (use prefab)
-        this.art1a = new Art(this, 0, 0, 'p1').setOrigin(0,0)
+
+        // Game Start
+        this.gameStarted = false;
+        this.add.text(game.config.width/2, game.config.height/2, 'START')
+            .setOrigin(0.5, 0.5)
+            .setStyle({ fontSize: 10 })
+            .setInteractive()
+            .on('pointerdown', () => {
+                this.gameStarted = true
+            });
+
+        // add art sprites here! (use prefab)
+        this.art1a = new Art(this, 135, 95, 'p1').setOrigin(0,0)
+        this.art1b = new Art(this, 150, 200, 'p1').setOrigin(0,0)
+        this.art1c = new Art(this, 20, 200, 'p1').setOrigin(0,0)
+        this.art1d = new Art(this, 40, 95, 'p1').setOrigin(0,0)
+
+        this.art2a = new Art(this, 45, 70, 'p2').setOrigin(0,0.5)
+        this.art2b = new Art(this, 180, 115, 'p2').setOrigin(0,0.5)
+        this.art2c = new Art(this, 30, 150, 'p2').setOrigin(0,0.5)
+
+        this.art3a = new Art(this, 100, 200, 'p3').setOrigin(0,0.5)
+        this.art3a = new Art(this, 220, 100, 'p3').setOrigin(0,0.5)
+
+        this.art4a = new Art(this, 90, 80, 'p4').setOrigin(0.5,0.5)
+        this.art4a = new Art(this, 190, 70, 'p4').setOrigin(0.5,0.5)
+        this.art4a = new Art(this, 180, 160, 'p4').setOrigin(0.5,0.5)
+        this.art4a = new Art(this, 80, 160, 'p4').setOrigin(0.5,0.5)
+
+            
         //add the rest and update placement
         this.add.sprite(0,0, 'fence').setOrigin(0,0)
-        this.flashlight = this.add.sprite(game.config.width/2, game.config.height/2, 'light').setOrigin(0.5, 0.5)
+
+        this.flashlight = 
+        this.add.sprite(game.config.width/2, game.config.height/2, 'light')
+                .setOrigin(0.5, 0.5)
+                              
+                              
     }
   
     update() {
-        if(this.input.x>=0&&this.input.x<=game.config.width){
+      
+        if(this.input.x >= 0 &&
+           this.input.x <= game.config.width && 
+           this.gameStarted == true){
             this.flashlight.x=this.input.x;
         }
      
-      if(this.input.y>=0&&this.input.y<=game.config.height){
+      if(this.input.y >= 0 &&
+         this.input.y <= game.config.height &&
+         this.gameStarted == true){
              this.flashlight.y=this.input.y;      
       }
+      
       
     }
   }
